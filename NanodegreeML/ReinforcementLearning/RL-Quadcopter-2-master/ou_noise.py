da3c39ea09b9320cd2229594d1e6e3afd34a8118
stella.y <stella.y@172.26.104.165>
2018-08-04 23:53:46 +0900

    replay buffer, ounoise

--- NanodegreeML/ReinforcementLearning/RL-Quadcopter-2-master/ou_noise.py
+++ NanodegreeML/ReinforcementLearning/RL-Quadcopter-2-master/ou_noise.py
@@ -1,38 +1,23 @@
-# --------------------------------------
-# Ornstein-Uhlenbeck Noise
-# Author: Flood Sung
-# Date: 2016.5.4
-# Reference: https://github.com/rllab/rllab/blob/master/rllab/exploration_strategies/ou_strategy.py
-# --------------------------------------
-
 import numpy as np
-import numpy.random as nr
+import copy
 
 class OUNoise:
-    """docstring for OUNoise"""
-    def __init__(self,action_dimension,mu=0, theta=0.15, sigma=0.2):
-        self.action_dimension = action_dimension
-        self.mu = mu
+    """Ornstein-Uhlenbeck process."""
+
+    def __init__(self, size, mu, theta, sigma):
+        """Initialize parameters and noise process."""
+        self.mu = mu * np.ones(size)
         self.theta = theta
         self.sigma = sigma
-        self.state = np.ones(self.action_dimension) * self.mu
         self.reset()
 
     def reset(self):
-        self.state = np.ones(self.action_dimension) * self.mu
+        """Reset the internal state (= noise) to mean (mu)."""
+        self.state = copy.copy(self.mu)
 
-    def noise(self):
+    def sample(self):
+        """Update internal state and return it as a noise sample."""
         x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * nr.randn(len(x))
+        dx = self.theta * (self.mu - x) + self.sigma * np.random.randn(len(x))
         self.state = x + dx
         return self.state
-
-if __name__ == '__main__':
-    ou = OUNoise(3)
-    states = []
-    for i in range(1000):
-        states.append(ou.noise())
-    import matplotlib.pyplot as plt
-
-    plt.plot(states)
-    plt.show()
